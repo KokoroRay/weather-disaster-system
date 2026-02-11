24ba3550a3986b4af78052d6faab942f1a791bd8
KokoroRay <namnguyen123456cm@gmail.com>
2026-02-11 23:40:40 +0700

    feat: Add initial user-service with Spring Boot, JPA entity, PostgreSQL configuration, and Docker setup.

--- services/user-service/src/main/java/com/wds/user_service/entity/User.java
+++ services/user-service/src/main/java/com/wds/user_service/entity/User.java
@@ -1,4 +1,22 @@
 package com.wds.user_service.entity;
 
-public class user {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String role = "USER";
 }
